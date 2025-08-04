5cb5a5d1baace8b7e3168577577f60a322565ddf
linaigao <linaigao@berkeley.edu>
2025-08-04 00:54:35 -0700

    Implement Navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ module.exports = {
         },
         secondary: "#282828",
         "gray-200": "#EAECF0",
-        "gray-300": "D0D5DD",
+        "gray-300": "#D0D5DD",
         "gray-500": "#667085",
         "gray-600": "#475467",
         "gray-700": "#344054",
