f84d1e1eeb02261196694a6e0fa81e971424963b
linaigao <linaigao@berkeley.edu>
2025-08-05 18:43:01 -0700

    Implement email using nodemailer

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import HeroCarousel from "@/components/HeroCarousel";
+import ProductCard from "@/components/ProductCard";
 import Searchbar from "@/components/Searchbar";
+import { getAllProducts } from "@/lib/actions";
 import Image from "next/image";
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
   return (
     <>
       <section className="px-6 md:px-20 py-24">
@@ -35,8 +38,8 @@ const Home = () => {
         <h2 className="section-text">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {["Apple iPhone 15 Plus", "Book", "Sneakers"].map((product) => (
-            <div key={product}>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
