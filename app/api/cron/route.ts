889c4b65db21e56bf440ab67dd9c7d331cb47b65
linaigao <linaigao@berkeley.edu>
2025-08-06 16:46:47 -0700

    Fix typos

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,7 +11,7 @@ import {
 import { NextResponse } from "next/server";
 
 export const maxDuration = 300;
-export const dymanic = "force-dynamic";
+export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export async function GET() {
